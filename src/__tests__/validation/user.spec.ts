69a24a146c4a50f7affd505c4f56e1b40fd68193
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-04-10 15:18:04 -0300

    test: add tests to user email validation

--- src/__tests__/validation/user.spec.ts
+++ src/__tests__/validation/user.spec.ts
@@ -13,4 +13,18 @@ describe('User Validation', () => {
     isValid = sut.validateName('User Name')
     expect(isValid).toBeTruthy()
   })
+
+  it('Should validate correctly an email', () => {
+    let isValid = sut.validateEmail('invalidmail.com')
+    expect(isValid).toBeFalsy()
+
+    isValid = sut.validateEmail('invalid@mail')
+    expect(isValid).toBeFalsy()
+
+    isValid = sut.validateEmail(null)
+    expect(isValid).toBeFalsy()
+
+    isValid = sut.validateEmail('user@mail.com')
+    expect(isValid).toBeTruthy()
+  })
 })
