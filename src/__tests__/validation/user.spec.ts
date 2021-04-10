d1d2c352ec12eeaede4bb9dc1a39ae53e69c3079
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-04-10 15:26:31 -0300

    test: add tests to user id validation

--- src/__tests__/validation/user.spec.ts
+++ src/__tests__/validation/user.spec.ts
@@ -38,4 +38,15 @@ describe('User Validation', () => {
     isValid = sut.validatePassword('_userpass')
     expect(isValid).toBeTruthy()
   })
+
+  it('Should validate correctly an UUID', () => {
+    let isValid = sut.validateId('invalid_uuid')
+    expect(isValid).toBeFalsy()
+
+    isValid = sut.validateId(null)
+    expect(isValid).toBeFalsy()
+
+    isValid = sut.validateId('bc7d7468-0fce-468c-8a02-587bc1eb9f77')
+    expect(isValid).toBeTruthy()
+  })
 })
