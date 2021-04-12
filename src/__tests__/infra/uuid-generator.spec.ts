8b9e998a4210855a7edfe7c8b931e5ca698d9c81
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-04-12 16:31:46 -0300

    test: increase uuid generator tests

--- src/__tests__/infra/uuid-generator.spec.ts
+++ src/__tests__/infra/uuid-generator.spec.ts
@@ -5,8 +5,9 @@ describe('UUID Generator', () => {
 
   it('Should generate a valid UUID v4', () => {
     const uuid = sut.generate()
+    expect(typeof uuid).toBe('string')
     // uuid v4 pattern
     const pattern = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
-    expect(pattern.test(uuid)).toBeTruthy()
+    expect(uuid).toMatch(pattern)
   })
 })
