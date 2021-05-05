c0ac7bf6ca6e6a56848b7d85adac14fd5bc38b3d
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-05-05 14:36:18 -0300

    refactor: improve argon2 and uuid generator tests

--- tests/unit/infra/utils/argon2-hasher.test.ts
+++ tests/unit/infra/utils/argon2-hasher.test.ts
@@ -1,22 +1,24 @@
 import { Argon2Hasher } from '@/infra/utils/argon2-hasher'
+import argon2 from 'argon2'
 
-import { verify, hash } from 'argon2'
+jest.mock('argon2')
 
 describe('Argon2 Hasher', () => {
+  const mockArgon2 = argon2 as jest.Mocked<typeof argon2>
   const sut = new Argon2Hasher()
   const payload = 'fake_payload'
 
   describe('Generate', () => {
-    it('Should generate a valid hash verified by argon2 on success', async () => {
+    mockArgon2.hash.mockResolvedValue('hash')
+    mockArgon2.verify.mockResolvedValue(true)
+
+    it('Should generate a valid hash on success', async () => {
       const hash = await sut.generate(payload)
-      const verified = verify(hash, payload)
-      expect(verified).toBeTruthy()
+      expect(hash).toBe('hash')
     })
 
     it('Should throw if argon2 hasher throws', async () => {
-      jest
-        .spyOn(sut, 'generate')
-        .mockImplementationOnce(() => new Promise((resolve, reject) => reject(new Error())))
+      mockArgon2.hash.mockRejectedValueOnce(new Error())
       const error = sut.generate(payload)
       await expect(error).rejects.toThrow()
     })
@@ -24,15 +26,20 @@ describe('Argon2 Hasher', () => {
 
   describe('Comparer', () => {
     it('Should return true when compare succeeds', async () => {
-      const validHash = await hash(payload)
-      const isValid = await sut.compare(payload, validHash)
+      const isValid = await sut.compare(payload, 'any_hash')
       expect(isValid).toBe(true)
     })
 
     it('Should return false when compare fails', async () => {
-      const invalidHash = await hash('different_payload')
-      const isValid = await sut.compare(payload, invalidHash)
+      mockArgon2.verify.mockResolvedValue(false)
+      const isValid = await sut.compare(payload, 'any_hash')
       expect(isValid).toBe(false)
+    })
+
+    it('Should throw if argon2 verify throws', async () => {
+      mockArgon2.verify.mockRejectedValueOnce(new Error())
+      const error = sut.compare(payload, 'any_hash')
+      await expect(error).rejects.toThrow()
     })
   })
 })
--- tests/unit/infra/utils/uuid-generator.test.ts
+++ tests/unit/infra/utils/uuid-generator.test.ts
@@ -5,7 +5,7 @@ describe('UUID Generator', () => {
 
   it('Should generate a valid UUID v4', () => {
     const uuid = sut.generate()
-    expect(typeof uuid).toBe('string')
+    expect(uuid).toEqual(expect.any(String))
     // uuid v4 pattern
     const pattern = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
     expect(uuid).toMatch(pattern)
