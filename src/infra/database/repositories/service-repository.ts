49366618b17d7dfab272e8712e84eec721a89860
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-05-12 14:38:56 -0300

    feat(delete-service): add delete and exists methods to service repositroy implementation

--- src/infra/database/repositories/service-repository.ts
+++ src/infra/database/repositories/service-repository.ts
@@ -1,13 +1,29 @@
 import { Service } from '@/domain/service/service'
 import { ServiceRepository } from '@/usecases/ports/service-repository'
-import { EntityRepository, Repository } from 'typeorm'
+import { getRepository } from 'typeorm'
 import { ServiceModel } from '../models/service'
 
-@EntityRepository(ServiceModel)
-export class PgServiceRepository extends Repository<Service> implements ServiceRepository {
+export class PgServiceRepository implements ServiceRepository {
   public async add(data: Service): Promise<Service> {
-    const service = this.create(data)
-    await this.save(service)
+    const repository = getRepository(ServiceModel)
+    const service = repository.create(data)
+    await repository.save(service)
     return service
+  }
+
+  public async exists(criteria: { id?: string; apiKey?: string }): Promise<boolean> {
+    const repository = getRepository(ServiceModel)
+    const { id, apiKey } = criteria
+
+    if (id) return !!repository.findOne({ where: { id } })
+    else if (apiKey) return !!repository.findOne({ where: { apiKey: apiKey } })
+  }
+
+  public async delete(criteria: { id?: string; apiKey?: string }): Promise<void> {
+    const repository = getRepository(ServiceModel)
+    const { id, apiKey } = criteria
+
+    if (id) await repository.delete({ id })
+    else if (apiKey) await repository.delete({ apiKey })
   }
 }
--- tests/unit/infra/database/service-repository.test.ts
+++ tests/unit/infra/database/service-repository.test.ts
@@ -1,9 +1,10 @@
+import { ServiceModel } from '@/infra/database/models/service'
+import { UserModel } from '@/infra/database/models/user'
 import pgConnectionHelper from '@/infra/database/pg-helper'
 import { PgServiceRepository } from '@/infra/database/repositories/service-repository'
 import { resolve } from 'path'
+import { createConnection, getRepository } from 'typeorm'
 import { fakeService } from '../../../mocks/service'
-import { createConnection, getCustomRepository } from 'typeorm'
-import { PgUserRepository } from '@/infra/database/repositories/user-repository'
 import { fakeUser } from '../../../mocks/user'
 
 describe('Pg Service Repository', () => {
@@ -15,48 +16,111 @@ describe('Pg Service Repository', () => {
     })
   })
 
+  const sut = new PgServiceRepository()
+
   beforeAll(async () => await pgConnectionHelper.connect())
   afterAll(async () => await pgConnectionHelper.close())
   beforeEach(() => {
-    getCustomRepository(PgServiceRepository).delete({})
-    getCustomRepository(PgUserRepository).delete({})
+    getRepository(ServiceModel).delete({})
+    getRepository(UserModel).delete({})
   })
 
-  describe('Add User', () => {
-    it('Should add a store a service data on success', async () => {
-      await getCustomRepository(PgUserRepository).add(fakeUser)
-      const sut = getCustomRepository(PgServiceRepository)
+  describe('Add Service', () => {
+    it('Should store a service data on success', async () => {
+      await getRepository(UserModel).save(fakeUser)
       const service = await sut.add(fakeService)
-
-      const data = await sut.findOne({ id: fakeService.id }) // native typeorm repository method
+      const data = await getRepository(ServiceModel).findOne({ id: fakeService.id })
       expect(data.id).toEqual(service.id)
     })
 
     it('Should not add a store a service if there is no respective maintainer', async () => {
-      const service = getCustomRepository(PgServiceRepository).add({
-        ...fakeService,
-        maintainer: null
-      })
+      const service = new PgServiceRepository().add({ ...fakeService, maintainer: null })
       await expect(service).rejects.toThrow()
     })
 
-    it('Should remove a service if its maintainer is removed', async () => {
-      const userRepo = getCustomRepository(PgUserRepository)
-      await userRepo.add(fakeUser)
+    it('Should throw if typeorm repository throws', async () => {
+      jest.spyOn(sut, 'add').mockRejectedValueOnce(new Error())
+      const error = sut.add(null)
+      await expect(error).rejects.toThrow()
+    })
+  })
 
-      const sut = getCustomRepository(PgServiceRepository)
+  describe('Delete service', () => {
+    it('Should remove a service if its maintainer is removed', async () => {
+      const userRepo = getRepository(UserModel)
+      await userRepo.save(fakeUser)
+
       const service = await sut.add(fakeService)
       expect(service.id).toEqual(fakeService.id) // inserted successfully
 
       userRepo.delete({ id: fakeUser.id })
-      const notFound = await sut.findOne({ id: fakeService.id, apiKey: fakeService.apiKey })
+      const notFound = await getRepository(ServiceModel).findOne({
+        id: fakeService.id,
+        apiKey: fakeService.apiKey
+      })
       expect(notFound).toBeFalsy() // removed if maintainer is removed (returns undefined)
     })
 
-    it('Should throw if typeorm repository throws', async () => {
-      const sut = getCustomRepository(PgServiceRepository)
-      jest.spyOn(sut, 'add').mockRejectedValueOnce(new Error())
+    it('Should delete a service by id on success', async () => {
+      await getRepository(UserModel).save(fakeUser)
+      const service = await sut.add(fakeService)
+      const { id } = await getRepository(ServiceModel).findOne({ id: fakeService.id })
+      expect(id).toEqual(service.id)
 
+      await sut.delete({ id })
+      const deleted = await getRepository(ServiceModel).findOne({ id: fakeService.id })
+      expect(deleted).toBeFalsy()
+    })
+
+    it('Should delete a service by apiKey on success', async () => {
+      await getRepository(UserModel).save(fakeUser)
+      const service = await sut.add(fakeService)
+      const { apiKey } = await getRepository(ServiceModel).findOne({ apiKey: fakeService.apiKey })
+      expect(apiKey).toEqual(service.apiKey)
+
+      await sut.delete({ apiKey })
+      const deleted = await getRepository(ServiceModel).findOne({ id: fakeService.id })
+      expect(deleted).toBeFalsy()
+    })
+
+    it('Should throw if typeorm repository throws', async () => {
+      jest.spyOn(sut, 'delete').mockRejectedValueOnce(new Error())
+      const error = sut.delete(null)
+      await expect(error).rejects.toThrow()
+    })
+  })
+
+  describe('Find for existing service', () => {
+    it('Should return true if found a service by apiKey', async () => {
+      await getRepository(UserModel).save(fakeUser)
+      const { apiKey } = await sut.add(fakeService)
+      const existingService = await sut.exists({ apiKey })
+      expect(existingService).toBeTruthy()
+    })
+
+    it('Should return true if found a service by id', async () => {
+      await getRepository(UserModel).save(fakeUser)
+      const { id } = await sut.add(fakeService)
+      const existingService = await sut.exists({ id })
+      expect(existingService).toBeTruthy()
+    })
+
+    it('Should return false if not found a service by id', async () => {
+      await getRepository(UserModel).save(fakeUser)
+      const { id } = await sut.add(fakeService)
+      const existingService = await sut.exists({ id })
+      expect(existingService).toBeTruthy()
+    })
+
+    it('Should return false if not found a service by apiKey', async () => {
+      await getRepository(UserModel).save(fakeUser)
+      const { apiKey } = await sut.add(fakeService)
+      const existingService = await sut.exists({ apiKey })
+      expect(existingService).toBeTruthy()
+    })
+
+    it('Should throw if typeorm repository throws', async () => {
+      jest.spyOn(sut, 'exists').mockRejectedValueOnce(new Error())
       const error = sut.add(null)
       await expect(error).rejects.toThrow()
     })
