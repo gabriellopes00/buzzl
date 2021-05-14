53a12619187df23be597467ff60294b1ec547218
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-05-14 10:24:45 -0300

    refactor: add helmet secure headers

--- src/app/setup/app.ts
+++ src/app/setup/app.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import serviceRoutes from '../routes/service'
 import userRoutes from '../routes/user'
-import { bodyParser, contentType, cors, noCache, rateLimiter } from './middlewares'
+import { bodyParser, contentType, cors, noCache, rateLimiter, secureHeaders } from './middlewares'
 
 const app = express()
 
-app.use([bodyParser, contentType, noCache, cors, rateLimiter])
+app.use([secureHeaders, bodyParser, contentType, noCache, cors, rateLimiter])
 app.use([userRoutes, serviceRoutes])
 
 export default app
