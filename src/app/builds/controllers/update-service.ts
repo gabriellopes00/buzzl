513bde21501029652d63b6d7fa6cd359b548122a
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-05-15 12:17:37 -0300

    feat(update-service): create update service controller composition

---
+++ src/app/builds/controllers/update-service.ts
@@ -0,0 +1,17 @@
+import { PgServiceRepository } from '@/infra/database/repositories/service-repository'
+import { UpdateServiceController } from '@/presentation/controllers/service/update-service'
+import { ApiKeyValidator } from '@/presentation/validation/api-key-validator'
+import { ValidatorCompositor } from '@/presentation/validation/compositor'
+import { RequiredFieldValidation } from '@/presentation/validation/required-fields'
+import { DbUpdateService } from '@/usecases/service/update-service'
+import { makeDecorator } from './factory'
+
+const requiredFieldsValidation = new RequiredFieldValidation(['apiKey', 'data'])
+const apiKeyValidator = new ApiKeyValidator()
+const validator = new ValidatorCompositor([requiredFieldsValidation, apiKeyValidator])
+
+const dbUpdateService = new DbUpdateService(new PgServiceRepository())
+
+export const updateServiceController = makeDecorator(
+  new UpdateServiceController(validator, dbUpdateService)
+)
--- src/app/routes/service.ts
+++ src/app/routes/service.ts
@@ -3,11 +3,13 @@ import { middlewareAdapter } from '../adapters/express-middlewares'
 import { routerAdapter } from '../adapters/express-router'
 import { addServiceController } from '../builds/controllers/add-service'
 import { deleteServiceController } from '../builds/controllers/delete-service'
+import { updateServiceController } from '../builds/controllers/update-service'
 import { authMiddleware } from '../builds/middlewares/auth-user'
 
 const router = Router()
 
 router.post('/service', middlewareAdapter(authMiddleware), routerAdapter(addServiceController))
+router.put('/service', middlewareAdapter(authMiddleware), routerAdapter(updateServiceController))
 router.delete('/service', middlewareAdapter(authMiddleware), routerAdapter(deleteServiceController))
 
 export default router
