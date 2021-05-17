8b2a0d9f2908846989ed5b2716c1c5ff3070f988
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-05-17 14:34:48 -0300

    refactor(list-service-by-user): change list service by user usecase params and dependencies

--- src/domain/service/list-service-by-user.ts
+++ src/domain/service/list-service-by-user.ts
@@ -1,6 +1,5 @@
-import { UnregisteredEmailError } from '../user/errors/unregistered-email'
 import { Service } from './service'
 
 export interface ListServiceByUser {
-  list(userEmail: string): Promise<Service[] | UnregisteredEmailError>
+  list(userId: string): Promise<Service[]>
 }
--- src/usecases/service/list-service-by-user.ts
+++ src/usecases/service/list-service-by-user.ts
@@ -1,21 +1,12 @@
 import { ListServiceByUser } from '@/domain/service/list-service-by-user'
 import { Service } from '@/domain/service/service'
-import { UnregisteredEmailError } from '@/domain/user/errors/unregistered-email'
 import { ServiceRepository } from '../ports/service-repository'
-import { UserRepository } from '../ports/user-repository'
 
 export class DbListServiceByUser implements ListServiceByUser {
-  constructor(
-    private readonly userRepository: UserRepository,
-    private readonly serviceRepository: ServiceRepository
-  ) {}
+  constructor(private readonly serviceRepository: ServiceRepository) {}
 
-  public async list(userEmail: string): Promise<Service[] | UnregisteredEmailError> {
-    const user = await this.userRepository.findOne({ email: userEmail })
-    if (!user) return new UnregisteredEmailError(userEmail)
-
-    const services = await this.serviceRepository.findAll({ maintainer: user.id })
-    if (!services) return null
-    else return services
+  public async list(userId: string): Promise<Service[]> {
+    const services = await this.serviceRepository.findAll({ maintainer: userId })
+    return services
   }
 }
