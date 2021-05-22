1d26c8f209d9124372c0d326c9709eebeed710ff
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-05-22 12:05:23 -0300

    feat(change-password): create controller composition

---
+++ src/app/builds/controllers/user/change-password.ts
@@ -0,0 +1,14 @@
+import { PgUserRepository } from '@/infra/database/repositories/user-repository'
+import { Argon2Hasher } from '@/infra/utils/argon2-hasher'
+import { ChangePassController } from '@/presentation/controllers/user/change-password'
+import { ValidatorCompositor } from '@/presentation/validation/compositor'
+import { RequiredFieldValidation } from '@/presentation/validation/required-fields'
+import { DbChangePassword } from '@/usecases/user/change-password'
+import { makeDecorator } from '../factory'
+
+const requiredFieldsValidation = new RequiredFieldValidation(['newPass', 'currentPass'])
+const validator = new ValidatorCompositor([requiredFieldsValidation])
+
+const dbChangePass = new DbChangePassword(new PgUserRepository(), new Argon2Hasher())
+
+export const changePassController = makeDecorator(new ChangePassController(validator, dbChangePass))
--- src/app/routes/user.ts
+++ src/app/routes/user.ts
@@ -4,6 +4,7 @@ import { routerAdapter } from '../adapters/express-router'
 import { addUserController } from '../builds/controllers/add-user'
 import { deleteUserController } from '../builds/controllers/delete-user'
 import { signInController } from '../builds/controllers/sign-in'
+import { changePassController } from '../builds/controllers/user/change-password'
 import { authMiddleware } from '../builds/middlewares/auth-user'
 
 const router = Router()
@@ -11,5 +12,10 @@ const router = Router()
 router.post('/signup', routerAdapter(addUserController))
 router.post('/signin', routerAdapter(signInController))
 router.delete('/user', middlewareAdapter(authMiddleware), routerAdapter(deleteUserController))
+router.put(
+  '/user/update/password',
+  middlewareAdapter(authMiddleware),
+  routerAdapter(changePassController)
+)
 
 export default router
