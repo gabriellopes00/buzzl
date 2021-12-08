ab9889b6d1ebfe0d212c350a9784edb3c93a01d6
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-12-08 15:17:30 -0300

    temp: remove unused code

--- src/app/setup/app.ts
+++ src/app/setup/app.ts
@@ -1,7 +1,4 @@
-// import { passport } from '@/infra/oauth/passport-google'
 import express from 'express'
-import session from 'express-session'
-import passport from 'passport'
 import { serve, setup } from 'swagger-ui-express'
 import docs from '../docs'
 import { AccountRoutes } from '../routes/account'
@@ -10,17 +7,17 @@ import { bodyParser, contentType, cors, noCache, rateLimiter, secureHeaders } fr
 
 const app = express()
 
-app.use(
-  session({
-    cookie: { secure: true, maxAge: 60000 },
-    secret: process.env.COOKIE_SECRET,
-    saveUninitialized: true,
-    resave: false
-  })
-)
-app.set('trust proxy', 1)
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(
+//   session({
+//     cookie: { secure: true, maxAge: 60000 },
+//     secret: process.env.COOKIE_SECRET,
+//     saveUninitialized: true,
+//     resave: false
+//   })
+// )
+// app.set('trust proxy', 1)
+// app.use(passport.initialize())
+// app.use(passport.session())
 app.use('/docs', serve, setup(docs))
 app.use([secureHeaders, bodyParser, contentType, noCache, cors, rateLimiter])
 
